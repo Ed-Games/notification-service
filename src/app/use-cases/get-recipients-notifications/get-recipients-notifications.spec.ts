3eab10ba9df9a9c5f544dc7348050b30b576de06
Ed-Games <edmarcosfilho2015@gmail.com>
2023-01-04 21:08:50 -0300

    create: tests for getting all notifications associated with a recipient id

---
+++ src/app/use-cases/get-recipients-notifications/get-recipients-notifications.spec.ts
@@ -0,0 +1,36 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationRepository } from '../../../../test/repositories/in-memory-notification-repository';
+import { GetRecipientNotifications } from './get-recipients-notifications';
+
+describe('Get recipients Notifications', () => {
+  it('should be able to get a recipient notifications', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const getRecipientNotifications = new GetRecipientNotifications(
+      notificationRepository,
+    );
+
+    await notificationRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+
+    await notificationRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+
+    await notificationRepository.create(
+      makeNotification({ recipientId: 'recipient-2' }),
+    );
+
+    const { notifications } = await getRecipientNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+      ]),
+    );
+  });
+});
